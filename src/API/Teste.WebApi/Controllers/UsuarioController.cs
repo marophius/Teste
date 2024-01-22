d762e2b0f48f2fa2231d34e710a772ba94606acc
Icaro Félix <ifeme08@gmail.com>
2024-01-22 16:16:50 -0300

    Adicionar arquivos de projeto.

---
+++ src/API/Teste.WebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Teste.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+    }
+}
---
+++ src/API/Teste.WebApi/Domain/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace Teste.WebApi.Domain
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Telefone { get; set; }
+        public string Senha { get; set; }
+    }
+}
